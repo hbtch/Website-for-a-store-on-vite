c83f0c1d37f66f6ee48b69a0cf06a6c189b08690
hbtch <gulnaz.baembitova@mail.ru>
2024-08-13 16:32:27 +0300

    lines with the plugin in the vite.config.js file have been removed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
-import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
   base: '/vite-project/',  
-  plugins: [
-    Inspect()
-  ],
-})
+});
